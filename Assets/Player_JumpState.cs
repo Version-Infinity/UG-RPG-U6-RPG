efefb18cfaf003e08822f52a7953d673aada28f1
Lord Vinja <LordVinja@versioninfinity.com>
2026-02-15 11:16:29 -0500

    feat: user controlled jump duration note: implemented without course, AI, or web searching - proud, but could be refined I am sure

--- Assets/Player_JumpState.cs
+++ Assets/Player_JumpState.cs
@@ -1,21 +1,36 @@
+using System;
 using UnityEngine;
 
 public class Player_JumpState : Player_AiredState
 {
     public Player_JumpState(Player player, StateMachine machine) : base(player, machine, "jumpFall")
     {
+        playerInputSet.Player.Jump.canceled += ctx => CheckForJumpCancelation();
     }
 
     override public void Enter()
     {
         base.Enter();
+        
         assignedPlayer.SetVelocity(playerRigidbody2D.linearVelocityX, assignedPlayer.JumpForce);
     }
 
     override public void Update()
     {
         base.Update();
+
         if (playerRigidbody2D.linearVelocityY < 0)
             assignedMachine.ChangeState(assignedPlayer.FallState);
+    }
+
+    private void CheckForJumpCancelation()
+    {
+       if(playerInputSet.Player.Jump.IsPressed())
+           Debug.Log("Still Jumping");
+        else { 
+            Debug.Log("Jump Canceled");
+            if (playerRigidbody2D.linearVelocityY > 0)
+                assignedPlayer.SetVelocity(playerRigidbody2D.linearVelocityX, -1);
+        }
     }
 }
